d23e26ac18be94460087c2ab136c30af295eccc0
Damian Justice <damian.justice@globalx.com.au>
2022-04-26 17:33:20 +1000

    Assignment Complete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,14 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
-
+  def edit
+    @article = Article.find(params[:id])
+  end
+  def update
+    @article = Article.find(params[:id])
+    @article.update(title: params[:article][:title], description: params[:aritcle][:description])
+    redirect_to article_path(@article)
+  end
   def show
     @article = Article.find(params[:id])
   end
